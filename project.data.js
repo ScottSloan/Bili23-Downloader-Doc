33bb92add0eaab1b6807f02e84e8c067dc26e0b1
ScottSloan <world1019@sina.com>
2026-06-22 21:28:42 +0800

    docs: 更新 v2.10.1 下载链接

--- project.data.js
+++ project.data.js
@@ -1,18 +1,18 @@
 export default {
   load() {
     return {
-      version: '2.10.0',
+      version: '2.10.1',
       date: '2026-06-22',
       lanzou: {
-        windows_exe_key: 'iP8a83slpmpa',
-        windows_zip_key: 'irk103slq08h',
-        windows_7_exe_key: 'iaUGm3slpsof',
-        linux_amd64_deb_key: 'iWg6D3slocgf',
-        linux_amd64_tar_gz_key: 'izQCx3slokqd',
-        linux_arm64_deb_key: 'iJxqQ3slormb',
-        linux_arm64_tar_gz_key: 'iwxw33sloyub',
-        macos_aarch64_key: 'iRnUH3slp82d',
-        macos_x86_64_key: 'iqIvl3slphfa'
+        windows_exe_key: 'iQb153smuifc',
+        windows_zip_key: 'i2qk53smunda',
+        windows_7_exe_key: 'i271n3smulbg',
+        linux_amd64_deb_key: 'iDFNg3smtu0d',
+        linux_amd64_tar_gz_key: 'ixnrs3smu1rc',
+        linux_arm64_deb_key: 'iFjR33smu4kd',
+        linux_arm64_tar_gz_key: 'iw9eK3smu9yh',
+        macos_aarch64_key: 'ipnum3smud4b',
+        macos_x86_64_key: 'iieDJ3smugej'
       }
     }
   }
