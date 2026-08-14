66bb3211ed638ff25a7b13b6359f8f6b64bae3c8
ScottSloan <world1019@sina.com>
2026-08-14 21:26:43 +0800

    docs: 更新 v2.14.0 下载链接

--- project.data.js
+++ project.data.js
@@ -1,18 +1,18 @@
 export default {
   load() {
     return {
-      version: '2.13.0',
-      date: '2026-08-12',
+      version: '2.14.0',
+      date: '2026-08-14',
       lanzou: {
-        windows_exe_key: 'iVJ1741zoeaj',
-        windows_zip_key: 'inGsS41zogpg',
-        windows_7_exe_key: 'iUBIQ41zof3i',
-        linux_amd64_deb_key: 'i5iS541znzgf',
-        linux_amd64_tar_gz_key: 'iyCqD41zo0za',
-        linux_arm64_deb_key: 'iaO0E41zo2jg',
-        linux_arm64_tar_gz_key: 'iPJR841zo4kj',
-        macos_aarch64_key: 'iyaIz41zo87a',
-        macos_x86_64_key: 'iZtqQ41zodad'
+        windows_exe_key: 'ipRrB42d8m5a',
+        windows_zip_key: 'i7aBR42d9f5e',
+        windows_7_exe_key: 'iJmrN42d8z1e',
+        linux_amd64_deb_key: 'iZivY42d5s4h',
+        linux_amd64_tar_gz_key: 'iF7Qv42d6cxg',
+        linux_arm64_deb_key: 'iI6e342d6rzi',
+        linux_arm64_tar_gz_key: 'iRcFf42d77la',
+        macos_aarch64_key: 'iDa6S42d7pli',
+        macos_x86_64_key: 'iXdwI42d88of'
       }
     }
   }
